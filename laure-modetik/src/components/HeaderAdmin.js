7d6f00e25e09e9a3c10bc6696bc3ec6794a9ed89
Njinga785 <lorandrat@hotmail.fr>
2020-12-29 15:38:28 +0100

    headers

--- laure-modetik/src/components/HeaderAdmin.js
+++ laure-modetik/src/components/HeaderAdmin.js
@@ -1,33 +1,42 @@
 import React, { Component } from 'react'
-import {NavLink} from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import Nav from 'react-bootstrap/Nav';
 
-export class HeaderAdmin extends Component { 
+export class HeaderAdmin extends Component {
     constructor(props) {
         super(props)
-        this.state= {
+        this.state = {
             name: this.props.name,
             profile: this.props.profile
         }
 
-    //     this.SignOut = this.SignOut.bind(this)
-    // }
+        //     this.SignOut = this.SignOut.bind(this)
+        // }
 
-    // SignOut() {
-    //     localStorage.clear()
-    //     this.props.signOut()
+        // SignOut() {
+        //     localStorage.clear()
+        //     this.props.signOut()
     }
     render() {
         return (
             <header className='black'>
-            <Nav.Item as="li">
-            <NavLink className="white"  activeClassName="active" exact to="/productsList">Products List</NavLink>
-        </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link as={Link} to="#">Nom du Site</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link as={Link} to="/sign-upAdmin">SignUpAdmin</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link as={Link} to="/sign-inAdmin">SignInAdmin</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link as={Link} to="/addcategorie">AddCategorie</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link as={Link} to="/addproduct">AddProduct</Nav.Link>
+                </Nav.Item>
 
-        <Nav.Item as="li">
-            <NavLink className="white"  activeClassName="active" exact to="/addproduct">Add a product</NavLink>
-        </Nav.Item>
-               
+
             </header>
         )
     }
--- laure-modetik/src/components/HeaderHome.js
+++ laure-modetik/src/components/HeaderHome.js
@@ -9,23 +9,20 @@ export class HeaderHome extends Component {
         return (
             <header className='black'>
                 <Nav.Item>
+                    <Nav.Link as={Link} to="#">Nom du Site</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link as={Link} to="/">Products List</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
                     <Nav.Link as={Link} to="/sign-up">SignUp</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link as={Link} to="/sign-in">SignInClients</Nav.Link>
+                    <Nav.Link as={Link} to="/sign-inClients">SignInClients</Nav.Link>
                 </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link as={Link} to="/sign-upAdmin">SignUpAdmin</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link as={Link} to="/sign-inAdmin">SignInAdmin</Nav.Link>
-                </Nav.Item> 
-                <Nav.Item>
-                    <Nav.Link as={Link} to="/addcategorie">AddCategorie</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link as={Link} to="/addproduct">AddProduct</Nav.Link>
-                </Nav.Item>
+                
+
+
 
             </header>
         )
